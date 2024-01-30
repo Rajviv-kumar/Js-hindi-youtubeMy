bd147381dacc2cf9c0ea46e0ee0ef764c93702f2
Rajviv-kumar <121580315+Rajviv-kumar@users.noreply.github.com>
2024-01-30 17:38:39 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all JS code as newer version
+
+//alert(3 +3 ) // we are using nodejs, not browser
+
+let name = "vivek";
+let age  = 18;
+let isLoggedIn = false;
+
+// number => 2 to the power 53
+// bigInt
+// string => "",''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Ram")
+
+console.log(typeof null);
+
+console.log(typeof undefined);
