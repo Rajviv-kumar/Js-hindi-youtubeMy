8fab972858cc71d05185ab2704785950ea994be3
Rajviv-kumar <121580315+Rajviv-kumar@users.noreply.github.com>
2024-01-30 18:10:19 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score    = "33";
+  score = "33abc"
+  score = null;
+  score = undefined;
+  score = true;
+  score = false;
+
+  score = "Hitesh";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof (valueInNumber));
+
+console.log(valueInNumber);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// " " => false; 
+// "vivek" => true;
+
+
+let number = 33;
+
+let stringNumber = String(number);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
